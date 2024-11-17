73f48769bc0a6ce89dc5bdcd8890f575eeb3bf2c
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2024-11-17 23:49:26 +0530

    level is showing in the heading

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const userclickedpattern =[]
 const buttoncolours = ["red", "blue", "green", "yellow"];
 const gamepattern = [];
+var level = 0;
 $(".btn").on("click",function(){
     var userchosencolour = $(this).attr("id");
     userclickedpattern.push(userchosencolour);
@@ -8,6 +9,8 @@ $(".btn").on("click",function(){
     animatePress(userchosencolour);
 })
 function nextsequence() {
+  level++;
+  $("#level-title").text("Level " + level)
     var randomnumber = Math.floor(Math.random() * 4);
   var randomchosencolour = buttoncolours[randomnumber];
   gamepattern.push(randomchosencolour);
@@ -16,13 +19,22 @@ function nextsequence() {
   audio.play();
 }
 $(document).ready(function () {
+  let keypressed = false;
+  let startButtonPressed = false;
     $("#start-btn").on("click", function() {
         startButtonPressed = true;  
          if (startButtonPressed){
+          $("#level-title").text("Level " + level);
          nextsequence();
-         };
-})}
-);
+         }});
+         $(document).keydown(function() {
+      keypressed =true;
+      if(keypressed&&startButtonPressed){
+        $("#level-title").text("Level " + level);
+        nextsequence();
+      }
+    })
+    });
 function playsound(randomchosencolour){
     var audio = new Audio("sounds/" + randomchosencolour + ".mp3");
   audio.play();
